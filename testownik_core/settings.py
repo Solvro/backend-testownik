0d952a3cd9f4867889e2331ce588b8c316a877ce
Antoni Czaplicki <antekczaplicki@gmail.com>
2025-01-10 12:28:33 +0100

    Update JWT token refreshing rules

--- testownik_core/settings.py
+++ testownik_core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 import dotenv
@@ -72,6 +72,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+}
+
+
+SIMPLE_JWT = {
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
 }
 
 
