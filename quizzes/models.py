bde469f1f55d4d4e79c25c963121178be84ab240
Antoni Czaplicki <antekczaplicki@gmail.com>
2025-01-17 16:08:41 +0100

    Change default quiz visibility

--- quizzes/models.py
+++ quizzes/models.py
@@ -18,7 +18,7 @@ class Quiz(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     maintainer = models.ForeignKey(User, on_delete=models.CASCADE)
-    visibility = models.PositiveIntegerField(choices=QUIZ_VISIBILITY_CHOICES, default=1)
+    visibility = models.PositiveIntegerField(choices=QUIZ_VISIBILITY_CHOICES, default=2)
     allow_anonymous = models.BooleanField(
         default=False,
         help_text="Każdy, nawet niezalogowany użytkownik będzie mógł wyświetlić tę bazę wchodząc na link",
