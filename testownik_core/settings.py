bc4d665369f3e360b4a391df31834ca12eb2617c
Antoni Czaplicki <antekczaplicki@gmail.com>
2025-01-24 19:38:16 +0100

    Add more env options

--- testownik_core/settings.py
+++ testownik_core/settings.py
@@ -25,17 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mo_va&2*lmj8z2ymm5i##wze&u%#d=v1boeki1tc=7@9sn3ilf"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-mo_va&2*lmj8z2ymm5i##wze&u%#d=v1boeki1tc=7@9sn3ilf")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://testownik.live",
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 
 # Application definition
 
