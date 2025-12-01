5049a8c2facffa17486591a534a66cee665bbe50
Batirro <batirro34@protonmail.com>
2025-12-01 17:19:53 +0100

    feat: add services for email sending

---
+++ quizzes/services/notifications.py
@@ -0,0 +1,34 @@
+from django.core.mail import send_mail, send_mass_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+def notify_quiz_shared_to_users(quiz,user):
+    if not user.email:
+        return
+    subject = f'Quiz "{quiz.title}" został ci udostępniony'
+    message = render_to_string('emails/quiz_shared.html', {
+        'user': user,
+        'quiz': quiz,
+    })
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        html_message=message,
+        fail_silently=True,
+    )
+
+def notify_quiz_shared_to_groups(quiz, group):
+    users_with_email = group.members.exclude(email='').exclude(email__isnull=True)
+
+    messages = []
+    for user in users_with_email:
+        subject = f'Quiz "{quiz.title}" został ci udostępniony'
+        message = render_to_string('emails/quiz_shared.html', {
+        'user': user,
+        'quiz': quiz,
+    })
+        messages.append((subject, message, settings.DEFAULT_FROM_EMAIL, [user.email]))
+    if messages:
+        send_mass_mail(messages, fail_silently=True)
