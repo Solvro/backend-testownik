81ee348689191c418f822aa07eb2bf225478e5fe
JaroDomcio <281053@student.pwr.edu.pl>
2025-12-09 22:35:47 +0100

    feat: add user notification preferences

--- users/views.py
+++ users/views.py
@@ -351,6 +351,9 @@ class SettingsView(APIView):
                     "sync_progress": True,
                     "initial_reoccurrences": 3,
                     "wrong_answer_reoccurrences": 1,
+                    "notify_quiz_shared": False,
+                    "notify_bug_reported": False,
+                    "notify_marketing": True
                 },
             )
         ],
@@ -367,6 +370,9 @@ class SettingsView(APIView):
                 "sync_progress": user_settings.sync_progress,
                 "initial_reoccurrences": user_settings.initial_reoccurrences,
                 "wrong_answer_reoccurrences": user_settings.wrong_answer_reoccurrences,
+                "notify_quiz_shared": user_settings.notify_quiz_shared,
+                "notify_bug_reported": user_settings.notify_bug_reported,
+                "notify_marketing": user_settings.notify_marketing,
             }
         )
 
@@ -380,6 +386,9 @@ class SettingsView(APIView):
                     "sync_progress": {"type": "boolean"},
                     "initial_reoccurrences": {"type": "integer", "minimum": 1},
                     "wrong_answer_reoccurrences": {"type": "integer", "minimum": 0},
+                    "notify_quiz_shared": {"type": "boolean"},
+                    "notify_bug_reported": {"type": "boolean"},
+                    "notify_marketing": {"type": "boolean"}
                 },
                 "required": [],
             }
@@ -395,6 +404,9 @@ class SettingsView(APIView):
                     "sync_progress": True,
                     "initial_reoccurrences": 3,
                     "wrong_answer_reoccurrences": 2,
+                    "notify_quiz_shared": False,
+                    "notify_bug_reported": True,
+                    "notify_marketing": True
                 },
             )
         ],
@@ -410,6 +422,9 @@ class SettingsView(APIView):
         sync_progress = data.get("sync_progress")
         initial_reoccurrences = data.get("initial_reoccurrences")
         wrong_answer_reoccurrences = data.get("wrong_answer_reoccurrences")
+        notify_quiz_shared = data.get("notify_quiz_shared")
+        notify_bug_reported = data.get("notify_bug_reported")
+        notify_marketing = data.get("notify_marketing")
 
         if sync_progress is not None:
             user_settings.sync_progress = sync_progress
@@ -432,12 +447,43 @@ class SettingsView(APIView):
                     status=HttpResponseBadRequest.status_code,
                 )
 
+        if notify_quiz_shared is not None:
+            if isinstance(notify_quiz_shared, bool):
+                user_settings.notify_quiz_shared = notify_quiz_shared
+            else:
+                return Response(
+                    "notify_quiz_shared must be a boolean",
+                    status=HttpResponseBadRequest.status_code,
+                )
+
+        if notify_bug_reported is not None:
+            if isinstance(notify_bug_reported, bool):
+                user_settings.notify_bug_reported = notify_bug_reported
+            else:
+                return Response(
+                    "notify_bug_reported must be a boolean",
+                    status=HttpResponseBadRequest.status_code,
+                )
+
+        if notify_marketing is not None:
+            if isinstance(notify_marketing, bool):
+                user_settings.notify_marketing = notify_marketing
+            else:
+                return Response(
+                    "notify_marketing must be a boolean",
+                    status=HttpResponseBadRequest.status_code,
+                )
+
+
         user_settings.save()
         return Response(
             {
                 "sync_progress": user_settings.sync_progress,
                 "initial_reoccurrences": user_settings.initial_reoccurrences,
                 "wrong_answer_reoccurrences": user_settings.wrong_answer_reoccurrences,
+                "notify_quiz_shared": user_settings.notify_quiz_shared,
+                "notify_bug_reported": user_settings.notify_bug_reported,
+                "notify_marketing": user_settings.notify_marketing,
             }
         )
 
