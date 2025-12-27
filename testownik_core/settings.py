dc191062db445c2c03ee80255c25bacaa249251f
Antoni Czaplicki <56671347+Antoni-Czaplicki@users.noreply.github.com>
2025-12-27 23:00:39 +0100

    feat: implement standardized email sending functionality (#98)
    * feat: implement standardized email sending functionality and fix some bugs
    
    * fix: enhance email templates and improve email sending logic

--- testownik_core/settings.py
+++ testownik_core/settings.py
@@ -42,7 +42,7 @@ if os.getenv("ALLOW_CORS_PREVIEW", "False") == "True":
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
 
-FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:5173")
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:5173").rstrip("/")
 
 # Application definition
 
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "quizzes.apps.QuizzesConfig",
     "alerts.apps.AlertsConfig",
     "maintenance.apps.MaintenanceConfig",
+    "testownik_core.apps.TestownikCoreConfig",
     "constance",
     "constance.backends.database",
     "rest_framework",
