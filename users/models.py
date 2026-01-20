b27fa4e605d3aa6882664386c0e009586c738e5f
Antoni Czaplicki <antekczaplicki@gmail.com>
2026-01-20 14:55:24 +0100

    feat: alter user email field to remove null and blank options and set default value

--- users/models.py
+++ users/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True)
     student_number = models.CharField(max_length=6)
     usos_id = models.IntegerField(null=True, blank=True)
     first_name = models.CharField(max_length=30)
