a95c07fba0413427d5149f7514218a3db735501c
matixi9 <290216@student.pwr.edu.pl>
2026-04-27 17:35:59 +0200

    refactor: remove alerts folder

--- alerts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from alerts.models import Alert
-
-
-class AlertSerializer(ModelSerializer):
-    class Meta:
-        model = Alert
-        fields = "__all__"
